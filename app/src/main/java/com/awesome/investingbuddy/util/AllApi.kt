0da8d8b9e0eee3d57f79099ee51eec2a39396342
kanavnayyer <78265974+kanavnayyer@users.noreply.github.com>
2024-06-29 22:03:10 +0530

    Update AllApi.kt

--- app/src/main/java/com/awesome/investingbuddy/util/AllApi.kt
+++ app/src/main/java/com/awesome/investingbuddy/util/AllApi.kt
@@ -4,9 +4,9 @@ import com.google.firebase.auth.FirebaseAuth
 
 class AllApi {
     companion object{
-         val apikCrypto="d3d39063-588d-4e31-b6b9-309c2c05cebd"
+         val apikCrypto=""// add api
 
         lateinit var auth: FirebaseAuth
-        val clientid:String="617287888801-17vq9odcl31t6ge336el1invloffmm8e.apps.googleusercontent.com"
+        val clientid:String=""// add  client id
     }
 }
